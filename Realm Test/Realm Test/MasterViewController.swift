88386661f9f441677c3fd1a426f461b1171b509b
Lucien Dupont <lucien@chromedomesoftware.com>
2017-04-23 14:22:00 -0700

    Delete items.

--- Realm Test/Realm Test/MasterViewController.swift
+++ Realm Test/Realm Test/MasterViewController.swift
@@ -109,6 +109,17 @@ class MasterViewController: UITableViewController {
 
 	override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
 		if editingStyle == .delete {
+			
+			let realm = try! Realm()
+			
+			// get the object that we are wanting to delete
+			let objectToDelete = objects?[indexPath.row]
+			
+			try! realm.write {
+				realm.delete(objectToDelete!)
+			}
+
+			
 		    // objects.remove(at: indexPath.row)
 		    // tableView.deleteRows(at: [indexPath], with: .fade)
 		} else if editingStyle == .insert {
