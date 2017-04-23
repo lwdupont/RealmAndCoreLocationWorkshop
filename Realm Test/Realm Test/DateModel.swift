45a930051dc662ca20f28695829b1017937e9dfc
Lucien Dupont <lucien@chromedomesoftware.com>
2017-04-23 13:35:35 -0700

    Initial dump of the stuff I’ve been working on.

---
+++ Realm Test/Realm Test/DateModel.swift
@@ -0,0 +1,14 @@
+//
+//  DateModel.swift
+//  Realm Test
+//
+//  Created by Lucien Dupont on 4/17/17.
+//  Copyright © 2017 Lucien Dupont. All rights reserved.
+//
+
+import Foundation
+import RealmSwift
+
+class DateModel : Object {
+	dynamic var date = NSDate()
+}
